de2c76ef28e9ed3acfeb90d4320e431ed162261e
Joseph Felker <jayefelker@gmail.com>
2023-04-18 15:23:32 -0500

    django auth setup

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
