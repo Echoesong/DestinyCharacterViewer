705986ed7a6fa04cdf992f2998c76658fbfe73a8
mgballou <matthewbuiltthat@gmail.com>
2023-04-19 15:25:25 -0400

    Building out models

--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,43 @@ class Profile(models.Model):
     
     def __str__(self):
         return f"belongs to {self.user}"
+    
+
+
+# class Ability(models.Model):
+#     bungie_api_ability_id = models.IntegerField()
+#     name = models.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Loadout(models.Model):
+#     # character_id = models.ForeignKey(Character, on_delete=models.CASCADE)
+#     name = models.CharField(max_length=255)
+#     # abilities = models.ManyToManyField(Ability)
+#     #there is no on_delete, do we need a different method than cascade for this?
+#     # weapon_kinetic = models.ForeignKey(Weapon)
+#     # weapon_energy = models.ForeignKey(Weapon)
+#     # weapon_heavy = models.ForeignKey(Weapon)
+#     # armor_head = models.ForeignKey(Armor)
+#     # armor_shoulder = models.ForeignKey(Armor)
+#     # armor_chest = models.ForeignKey(Armor)
+#     # armor_legs = models.ForeignKey(Armor)
+#     # armor_class = models.ForeignKey(Armor)
