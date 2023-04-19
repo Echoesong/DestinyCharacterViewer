c2af26c580f92131a919b8a2d5b3dfb617ec5c5d
mgballou <matthewbuiltthat@gmail.com>
2023-04-19 16:08:03 -0400

    Correct oauth2

--- destiny_loadout_manager/settings.py
+++ destiny_loadout_manager/settings.py
@@ -39,7 +39,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'main_app',
-    'django-oauth2_provider',
+    'django_oauth2_provider',
     'django_simple_bulma',
     'django.contrib.admin',
     'django.contrib.auth',
