cc70b6bed38d80d2cd6509ed4c28f49a5b4ec311
Joseph Felker <jayefelker@gmail.com>
2023-04-19 20:23:53 -0500

    make Weapon and Armor model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from enum import Enum
 
 # Create your models here.
 
@@ -10,6 +11,45 @@ class Profile(models.Model):
     expires_in = models.IntegerField()
     refresh_token = models.CharField(max_length=255, null=True, blank=True)  # This is for confidential clients only
     membership_id = models.CharField(max_length=255)
-    
+
     def __str__(self):
         return f"belongs to {self.user}"
+    
+WEAPON_SLOTS = (
+    ('Kin', 'Kinetic'),
+    ('En', 'Energy'),
+    ('Hev', 'Heavy')
+)
+
+ARMOR_SLOTS = (
+    ('H', 'Kinetic'),
+    ('A', 'Arms'),
+    ('Ch', 'Chest'),
+    ('L', 'Legs'),
+    ('Cl', 'Class')
+)
+
+
+class Weapon(models.Model):
+    id = models.IntegerField
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    # instance_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    # NOTE: default probably doesn't work for the app, as defaulting to that index location is nonsensical. Keeping for now
+    slot = models.CharField(max_length=3, choices=WEAPON_SLOTS, default=WEAPON_SLOTS[0][0])
+
+    def __str__(self):
+        return self.name
+
+class Armor(models.Model):
+    id = models.IntegerField
+    # user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    # instance_id = models.IntegerField()
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    # NOTE: ref: note in class Weapon
+    slot = models.CharField(max_length=100, choices=ARMOR_SLOTS, default=ARMOR_SLOTS[0][0])
+
+    def __str__(self):
+        return self.name
