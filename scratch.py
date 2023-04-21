6b06b2e75cd43cfb40b76eac12157f67c0918017
Joseph Felker <jayefelker@gmail.com>
2023-04-21 14:06:54 -0500

    current working branch

---
+++ scratch.py
@@ -0,0 +1,50 @@
+object = {
+"2305843009299059507": {
+        "membershipId": "4611686018467945369",
+        "membershipType": 3,
+        "characterId": "2305843009299059507",
+        "dateLastPlayed": "2023-04-09T23:11:30Z",
+        "minutesPlayedThisSession": "23",
+        "minutesPlayedTotal": "7316",
+        "light": 1814,
+        "stats": {
+            "1935470627": 1814,
+            "2996146975": 12,
+            "392767087": 104,
+            "1943323491": 44,
+            "1735777505": 38,
+            "144602215": 47,
+            "4244567218": 30
+        },
+        "raceHash": 3887404748,
+        "genderHash": 2204441813,
+        "classHash": 3655393761,
+        "raceType": 0,
+        "classType": 0,
+        "genderType": 1,
+        "emblemPath": "/common/destiny2_content/icons/7b7ae42d9effb84106c7a0d51bc30646.jpg",
+        "emblemBackgroundPath": "/common/destiny2_content/icons/807f54888b7d5e6ccc4386a587a366c0.jpg",
+        "emblemHash": 2939572586,
+        "emblemColor": {
+            "red": 26,
+            "green": 7,
+            "blue": 3,
+            "alpha": 255
+        },
+        "levelProgression": {
+            "progressionHash": 1716568313,
+            "dailyProgress": 0,
+            "dailyLimit": 0,
+            "weeklyProgress": 0,
+            "weeklyLimit": 0,
+            "currentProgress": 0,
+            "level": 50,
+            "levelCap": 50,
+            "stepIndex": 50,
+            "progressToNextLevel": 0,
+            "nextLevelAt": 0
+        },
+        "baseCharacterLevel": 50,
+        "percentToNextLevel": 0.0
+    } }
+
