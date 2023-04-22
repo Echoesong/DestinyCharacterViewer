c80d15fc785b4db24e6820c84045903ddd43d840
mgballou <matthewbuiltthat@gmail.com>
2023-04-22 13:18:48 -0400

    Update character template

--- main_app/models.py
+++ main_app/models.py
@@ -19,19 +19,6 @@ class Profile(models.Model):
     def __str__(self):
         return f"belongs to {self.user}"
 
-    
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    access_token = models.CharField(max_length=255)
-    token_type = models.CharField(max_length=255)
-    expires_in = models.IntegerField()
-    refresh_token = models.CharField(max_length=255, null=True, blank=True)  # This is for confidential clients only
-    membership_id = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"belongs to {self.user}"
-   
      
 class Race(models.Model):
     name = models.CharField(max_length=100)
