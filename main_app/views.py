3ed07d11c90f0118ad64ee4d5f8d30b40adf94c9
mgballou <matthewbuiltthat@gmail.com>
2023-04-23 11:26:02 -0400

    Implement additional styling - layout, centering, text weights. Refactor user flow through syncing profile

--- main_app/views.py
+++ main_app/views.py
@@ -28,7 +28,7 @@ def signup(request):
       
       # This is how we log a user in via code
       login(request, user)
-      return redirect('home')
+      return redirect('bungie_auth')
     else:
       error_message = 'Invalid sign up - try again'
   # A bad POST or a GET request, so render signup.html with an empty form
